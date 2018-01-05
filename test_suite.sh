d9ee6e6b122d6ec7a2ca9da2acb6e628ac6c5bb4
Victor Alenkov (victor@alenkov.name) <victor@alenkov.name>
2018-01-05 15:03:45 +0300

    0) fixed and refactoring Travis CI configuration 1) Added output of the command to be run test_suite

--- test_suite.sh
+++ test_suite.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+cleanup() {
+	rm -Rf ./build/ ./jvmw.properties ./tmp/
+}
+
 test_failure() {
-	rm -Rf ./build/ ./jvmw.properties
+	cleanup
 	echo "----"
 	echo "test failure: ${BASH_SOURCE[0]}:${FUNCNAME[1]}:${BASH_LINENO[0]}"
 	# shellcheck disable=SC2016
@@ -10,23 +14,26 @@ test_failure() {
 	exit 1
 }
 
-mkdir -p ./build/
+run() {
+	echo "${@:1}"
+	eval "${@:1}"
+}
 
-CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JDK_FAMILY='8' ./tests.sh || test_failure
-CHECK_JDK_VERSION='1.8.0_151' CHECK_JDK_HOME='jdk8' JDK_FAMILY='8' ./tests.sh || test_failure
-CHECK_JDK_VERSION='1.8.0_152' CHECK_JDK_HOME='jdk8u152' JDK_FAMILY='8' JDK_UPDATE_VERSION=152 ./tests.sh || test_failure
-CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JVMW_FILE='jvmw.8.properties' ./tests.sh || test_failure
-CHECK_JDK_VERSION='1.8.0_151' CHECK_JDK_HOME='jdk8' JVMW_FILE='jvmw.8.properties' ./tests.sh || test_failure
-CHECK_JDK_VERSION='1.8.0_152' CHECK_JDK_HOME='jdk8u152' JVMW_FILE='jvmw.8u152.properties' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JDK_FAMILY='8' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='1.8.0_151' CHECK_JDK_HOME='jdk8' JDK_FAMILY='8' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='1.8.0_152' CHECK_JDK_HOME='jdk8u152' JDK_FAMILY='8' JDK_UPDATE_VERSION=152 ./tests.sh || test_failure
+run CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JVMW_FILE='jvmw.8.properties' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='1.8.0_151' CHECK_JDK_HOME='jdk8' JVMW_FILE='jvmw.8.properties' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='1.8.0_152' CHECK_JDK_HOME='jdk8u152' JVMW_FILE='jvmw.8u152.properties' ./tests.sh || test_failure
 
-CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9' ./tests.sh || test_failure
-CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9' JDK_FAMILY='9' ./tests.sh || test_failure
-CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9' JVMW_FILE='jvmw.9.properties' ./tests.sh || test_failure
-CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9u152' JVMW_FILE='incorrect.jvmw.9u152.properties' ./tests.sh 2> /dev/null && test_failure
+run CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9' JDK_FAMILY='9' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9' JVMW_FILE='jvmw.9.properties' ./tests.sh || test_failure
+run CHECK_JDK_VERSION='9.0.1' CHECK_JDK_HOME='jdk9u152' JVMW_FILE='incorrect.jvmw.9u152.properties' ./tests.sh 2>/dev/null && test_failure
 
-rm -Rf ./build/* && cp ./jdkw ./build/jdkw
-CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JDK_FAMILY='8' ./tests.sh ./build/jdkw || test_failure
-rm -Rf ./build/* && cp ./jdkw ./build/jdkw
-CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JVMW_FILE='jvmw.8.properties' ./tests.sh ./build/jdkw || test_failure
+cleanup && mkdir -p ./tmp/ && cp ./jdkw ./tmp/jdkw
+run CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JDK_FAMILY='8' ./tests.sh ./tmp/jdkw || test_failure
+cleanup && mkdir -p ./tmp/ && cp ./jdkw ./tmp/jdkw
+run CHECK_JDK_VERSION='1.8.0' CHECK_JDK_HOME='jdk8' JVMW_FILE='jvmw.8.properties' ./tests.sh ./tmp/jdkw || test_failure
 
-rm -Rf ./build/
+cleanup
