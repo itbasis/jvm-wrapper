23da8d75411d1eee42a5a4d7216907b4a3dab4a0
Victor Alenkov <BorzdeG@users.noreply.github.com>
2018-01-24 12:29:24 +0300

    #15 Ability to disable debugging mode (#16)
    * #15 Ability to disable debugging mode
    * fixed tests
    * update README.md
    * Added the ability to specify REQUIRED_UPDATE from outside the script
    * Fixed bug with handling of empty lines in jvmw.properties
    * refactoring tests

--- tests.sh
+++ tests.sh
@@ -8,23 +8,25 @@ CHECK_JDK_ROOT_DIR=${CHECK_JDK_ROOT_DIR}
 LAST_UPDATE_FILE=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}.last_update
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 #
-rm -Rf ./build/ ./jvmw.properties "${LAST_UPDATE_FILE}"
-#
 
+function clean(){
+	rm -Rf ./build/ ./jvmw.properties ./jvmw.properties.bak "${LAST_UPDATE_FILE}"
+}
+#
 function log_cmd() {
 	>&2 echo --- cmd: "'$@'" --- ${BASH_SOURCE[0]}:${FUNCNAME[1]}:${BASH_LINENO[0]}
 }
 
 function log_test() {
-	>&2 echo --- test: "'$@'" --- ${BASH_SOURCE[0]}:${FUNCNAME[1]}:${BASH_LINENO[0]}
+	>&2 echo --- test: "$@" --- ${BASH_SOURCE[0]}:${FUNCNAME[1]}:${BASH_LINENO[1]}:${BASH_LINENO[0]}
 }
 
 function error() {
 	>&2 echo --- capture output :: begin ---
-	>&2 echo "$1"
+	>&2 echo "${output}"
 	>&2 echo --- capture output :: end ---
-	>&2 echo --- error :: ${BASH_SOURCE[0]}:${FUNCNAME[1]}:${BASH_LINENO[0]} ---
-	rm -Rf ./build/test/ > /dev/null 2>&1
+	>&2 echo --- error :: ${BASH_SOURCE[0]}:${FUNCNAME[1]}:${BASH_LINENO[1]}:${BASH_LINENO[0]} ---
+	clean
 	exit 1
 }
 
@@ -34,56 +36,96 @@ function check_output() {
 	log_test "check '$1' contains '$2'"
 	local -r cmd="${JVMW} $1 2>&1"
 	log_cmd "${cmd}"
-	local -r output=$(eval "${cmd}" 2>&1)
+	output=$(eval "${cmd}" 2>&1)
 	if [[ ${output} != *"$2"* ]]; then
-		error "${output}"
+		error
 	fi
-	echo "${output}"
+}
+
+function prepare_test(){
+	clean
+	if [ -f "samples/${JVMW_FILE}" ]; then
+		cp "samples/${JVMW_FILE}" jvmw.properties
+	fi
 }
 
 #
-if [ -f "samples/${JVMW_FILE}" ]; then
-	cp "samples/${JVMW_FILE}" jvmw.properties
+prepare_test
+echo -e "\nREQUIRED_UPDATE=N" >> jvmw.properties
+rm -Rf "${LAST_UPDATE_FILE}" ${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/
+check_output 'java -version' "java: No such file"
+log_test "check not call function 'otn_page_archive_jdk_parser'"
+if [[ ${output} == *"otn_page_archive_jdk_parser"* ]]; then
+	error
 fi
-#
-output=$(check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/")
 
+#
+prepare_test
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
+if [[ ${output} == *"[DEBUG]"* ]]; then
+	error
+fi
+
+prepare_test
+echo -e "\nJVMW_DEBUG=Y" >> jvmw.properties
+
+#
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
 log_test "check call function 'otn_page_archive_jdk_parser'"
 if [[ ${output} != *"otn_page_archive_jdk_parser"* ]]; then
-	error "${output}"
+	error
 fi
 
+#
 log_test "check not call function 'otn_page_archive_jdk_parser'"
-output=$(check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/" 2>&1)
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
 if [[ ${output} == *"otn_page_archive_jdk_parser"* ]]; then
-	error "${output}"
+	error
 fi
 log_test "check contains 'prev_date'"
 if [[ ${output} != *"prev_date="* ]]; then
-	error "${output}"
+	error
 fi
 
+#
 log_test "check call function 'otn_page_archive_jdk_parser'"
 fake_date=$([[ "${OS}" == "darwin" ]] && echo "$(date -v -2d +"%F %R"))" || echo "$(date --date="-2 days" '+%F %R')")
 printf "%s" "${fake_date}" >"${LAST_UPDATE_FILE}"
-output=$(check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/" 2>&1)
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
 if [[ ${output} != *"otn_page_archive_jdk_parser"* ]]; then
-	error "${output}"
+	error
 fi
 printf "%s\nfake" "${fake_date}" >"${LAST_UPDATE_FILE}"
-output=$(check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/" 2>&1)
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
 if [[ ${output} != *"otn_page_archive_jdk_parser"* ]]; then
-	error "${output}"
+	error
 fi
 printf "%s\nfake\nfake" "${fake_date}" >"${LAST_UPDATE_FILE}"
-output=$(check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/" 2>&1)
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
 if [[ ${output} != *"otn_page_archive_jdk_parser"* ]]; then
-	error "${output}"
+	error
 fi
 
-check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/" > /dev/null
-check_output 'javac -version' "${CHECK_JDK_VERSION}" > /dev/null
-check_output 'java -version' "${CHECK_JDK_VERSION}" > /dev/null
+#
+cp jvmw.properties jvmw.properties.bak
+echo 'REQUIRED_UPDATE=N' >> jvmw.properties
+printf "%s\nfake\nfake" "${fake_date}" >"${LAST_UPDATE_FILE}"
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
+log_test "check not call function 'otn_page_archive_jdk_parser'"
+if [[ ${output} == *"otn_page_archive_jdk_parser"* ]]; then
+	error
+fi
+log_test "check not contains 'prev_date'"
+if [[ ${output} == *"prev_date="* ]]; then
+	error
+fi
+cp jvmw.properties.bak jvmw.properties
+
+#
+
+check_output 'info' "JDK_HOME=${HOME}/.jvm/${CHECK_JDK_ROOT_DIR}/"
+check_output 'javac -version' "${CHECK_JDK_VERSION}"
+check_output 'java -version' "${CHECK_JDK_VERSION}"
 
 #
 mkdir -p ./build/test
@@ -95,5 +137,6 @@ if [[ ! -f "./build/test/Test.class" ]]; then
 	error "${output}"
 fi
 #
-check_output 'java -cp ./build/test/ Test' "${CHECK_JDK_VERSION}" > /dev/null
-rm -Rf ./build/test/
+check_output 'java -cp ./build/test/ Test' "${CHECK_JDK_VERSION}"
+#
+clean
