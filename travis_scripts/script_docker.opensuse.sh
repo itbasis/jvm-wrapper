9a4ac8a86b614eae57a26aac5b7fa33d82050bf5
Victor Alenkov <victor@alenkov.name>
2018-01-28 20:57:34 +0300

    #13 Support for updates for JDK versions 9 and above * fixed tests for Docker

--- travis_scripts/script_docker.opensuse.sh
+++ travis_scripts/script_docker.opensuse.sh
@@ -8,6 +8,6 @@ docker run \
 -e "ORACLE_PASSWORD=${ORACLE_PASSWORD}" \
 -v "$(pwd)/jdkw":/root/jdkw \
 -v "$(pwd)/test/":/root/test/ \
--v "$(pwd)/samples/":/root/samples/ \
+-v "$(pwd)/samples.properties/":/root/samples.properties/ \
 -v "$(pwd)/tests.sh":/root/tests.sh \
  $DOCKER_IMAGE:$DOCKER_TAG bash -c "zypper in -nfyl curl tar && cd /root && ./tests.sh 2>&1"
