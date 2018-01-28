d7d91cc1526875b157339be73f93011d96b6fa94
Victor Alenkov <BorzdeG@users.noreply.github.com>
2018-01-28 22:12:38 +0300

    #13 (#18)
    * #13 Support for updates for JDK versions 9 and above
    * refactoring tests
    * The variables `JVM_VERSION_MAJOR` and `JVM_VERSION_MINOR` are no longer read from the outside - the JVM version is passed to one variable `JVM_VERSION`
    * The variable `JDK_VERSION` was renamed to `JVM_VERSION`
    * For JVM version less than 8, the archive checksum is no longer checked. As a consequence, if a valid JVM archive is locally found, it will be removed and re-downloaded
    * After the distribution is downloaded and unpacked, the JVM version of the downloaded and required version is not checked - they are assumed to be the same

--- travis_scripts/script_docker.centos.sh
+++ travis_scripts/script_docker.centos.sh
@@ -8,7 +8,6 @@ docker run \
 -e "ORACLE_PASSWORD=${ORACLE_PASSWORD}" \
 -v "$(pwd)/jdkw":/root/jdkw \
 -v "$(pwd)/test/":/root/test/ \
--v "$(pwd)/samples/":/root/samples/ \
--v "$(pwd)/test_suite.sh":/root/test_suite.sh \
+-v "$(pwd)/samples.properties/":/root/samples.properties/ \
 -v "$(pwd)/tests.sh":/root/tests.sh \
- $DOCKER_IMAGE:$DOCKER_TAG bash -c "cd /root && ./test_suite.sh 2>&1"
+ $DOCKER_IMAGE:$DOCKER_TAG bash -c "cd /root && ./tests.sh 2>&1"
