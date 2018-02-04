7d9c3ddb602277e74fd9fab3651326e7f33caca3
Victor Alenkov <victor@alenkov.name>
2018-02-04 23:11:42 +0300

    #24 External command support
    * fixed Docker tests

--- travis_scripts/script_docker.opensuse.sh
+++ travis_scripts/script_docker.opensuse.sh
@@ -7,7 +7,7 @@ docker run \
 -e "ORACLE_USER=${ORACLE_USER}" \
 -e "ORACLE_PASSWORD=${ORACLE_PASSWORD}" \
 -v "$(pwd)/jdkw":/root/jdkw \
--v "$(pwd)/test/":/root/test/ \
+-v "$(pwd)/tests/":/root/tests/ \
 -v "$(pwd)/samples.properties/":/root/samples.properties/ \
 -v "$(pwd)/tests.sh":/root/tests.sh \
  $DOCKER_IMAGE:$DOCKER_TAG bash -c "zypper in -nfyl curl tar && cd /root && ./tests.sh 2>&1"
