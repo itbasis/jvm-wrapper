76e9e050580e5f824335dc0b1047abd34acf0dcc
Victor Alenkov <victor@alenkov.name>
2018-02-19 20:41:13 +0300

    Travis CI configuration optimization

---
+++ src/test/bash/_test_docker_suite.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Hack for code verification
+TEST_TYPE=${TEST_TYPE:?}
+TEST_FULL_VERSION=${TEST_FULL_VERSION:?}
+TEST_REUSE_JAVA_VERSION=${TEST_REUSE_JAVA_VERSION}
+DOCKER_IMAGES=${DOCKER_IMAGES:?}
+#
+
+rm -Rf ./build/
+mkdir -p ./build/
+cd ./build/ || exit 1
+
+CUR_DIR=./../$(dirname "$0")
+export CUR_DIR
+
+for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
+	docker_image=${DOCKER_IMAGE%%:*}
+	docker_image=${docker_image%%/*}
+	docker_script=${CUR_DIR}/travis_ci/script_docker.${docker_image}.sh
+	export DOCKER_IMAGE
+	echo ":: :: execute '${docker_script}'..."
+	${docker_script} || exit 1
+	echo ":: :: done..."
+done
--- src/test/bash/_test_suite.sh
+++ src/test/bash/_test_suite.sh
@@ -4,7 +4,6 @@
 TEST_TYPE=${TEST_TYPE:?}
 TEST_FULL_VERSION=${TEST_FULL_VERSION:?}
 TEST_REUSE_JAVA_VERSION=${TEST_REUSE_JAVA_VERSION}
-DOCKER_IMAGES=${DOCKER_IMAGES}
 #
 
 rm -Rf ./build/
@@ -17,14 +16,4 @@ export CUR_DIR
 for test_script in $(find "${CUR_DIR}" -name "${TEST_TYPE}.*.sh" -type f | sort); do
 	echo ":: execute '${test_script}'..."
 	${test_script} || exit 1
-done
-
-for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
-	docker_image=${DOCKER_IMAGE%%:*}
-	docker_image=${docker_image%%/*}
-	docker_script=${CUR_DIR}/travis_ci/script_docker.${docker_image}.sh
-	export DOCKER_IMAGE
-	echo ":: :: execute '${docker_script}'..."
-	${docker_script} || exit 1
-	echo ":: :: done..."
 done
