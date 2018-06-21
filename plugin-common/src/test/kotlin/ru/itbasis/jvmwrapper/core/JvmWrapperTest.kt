bcd1d8eb0e89095e3b6002dc99ab18d5c7d21288
Victor Alenkov <victor@alenkov.name>
2018-06-21 18:16:20 +0300

    * Linux support

--- plugin-common/src/test/kotlin/ru/itbasis/jvmwrapper/core/JvmWrapperTest.kt
+++ plugin-common/src/test/kotlin/ru/itbasis/jvmwrapper/core/JvmWrapperTest.kt
@@ -4,7 +4,6 @@ import io.kotlintest.Description
 import io.kotlintest.TestResult
 import io.kotlintest.data.forall
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.FunSpec
 import io.kotlintest.tables.row
 import org.junit.rules.TemporaryFolder
 import java.io.File
@@ -13,7 +12,7 @@ import java.math.RoundingMode
 import java.nio.charset.Charset
 import java.util.concurrent.TimeUnit
 
-internal class JvmWrapperTest : FunSpec() {
+internal class JvmWrapperTest : AbstractIntegrationTests() {
   private val stepListener: (String) -> Unit = { msg -> println(msg) }
   private val downloadProcessListener: (String, Long, Long) -> Boolean = { _, sizeCurrent, sizeTotal ->
     val percentage = BigDecimal(sizeCurrent.toDouble() / sizeTotal * 100).setScale(2, RoundingMode.HALF_UP)
@@ -46,11 +45,14 @@ internal class JvmWrapperTest : FunSpec() {
         File(System.getProperty("user.dir")).parentFile.resolve(JvmWrapper.SCRIPT_FILE_NAME)
           .copyTo(File(workingDir, JvmWrapper.SCRIPT_FILE_NAME))
 
-        val jvmWrapper = JvmWrapper(workingDir, stepListener = stepListener, downloadProcessListener = downloadProcessListener)
+        val jvmWrapper = JvmWrapper(
+          workingDir, stepListener = stepListener, downloadProcessListener = if (!launchedInCI()) downloadProcessListener else null
+        )
         val jvmHomeDir = jvmWrapper.jvmHomeDir
         println("jvmHomeDir: $jvmHomeDir")
 
         val jvmBinDir = jvmHomeDir.resolve("bin")
+        println("jvmBinDir: $jvmBinDir")
         jvmBinDir.exists() shouldBe true
 
         val process = ProcessBuilder(File(jvmBinDir, "java").absolutePath, "-fullversion").start()
