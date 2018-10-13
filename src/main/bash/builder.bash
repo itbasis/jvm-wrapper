f14f07e5731931b6e9779acb45e6094ceddebe4c
Victor Alenkov <victor@alenkov.name>
2018-10-14 00:42:06 +0300

    Added build date

--- src/main/bash/builder.bash
+++ src/main/bash/builder.bash
@@ -5,6 +5,10 @@ SCRIPT_FILE=build/jvmw
 mkdir -p build
 echo '#!/usr/bin/env bash' > ${SCRIPT_FILE}
 
+# push build date
+echo "# Build date: `date +%Y%m%d_%H%M%S`" >> ${SCRIPT_FILE}
+echo "#" >> ${SCRIPT_FILE}
+
 for f in 'core' 'properties' 'download' 'unpack' 'core_oracle' 'download_oracle' 'core_openjdk' 'download_openjdk' 'main'; do
 	awk '/^# BEGIN SCRIPT/{flag=1;next}/^# END SCRIPT/{flag=0}flag' <"src/main/bash/jvmw/${f}.bash" >> ${SCRIPT_FILE}
 done
